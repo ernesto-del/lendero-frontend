a45aae6b6b03f89b2e5ec4db9b718338965854bb
ernesto-del <ernesto@lenderocapital.com>
2026-02-17 19:46:55 -0600

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,14 +3,14 @@ import api from './api';
 export const authService = {
   login: async (email, password) => {
     const response = await api.post('/auth/login', { email, password });
-    return response.data;
+    return response.data.data;
   },
-
+  
   logout: async () => {
     const response = await api.post('/auth/logout');
     return response.data;
   },
-
+  
   me: async () => {
     const response = await api.get('/auth/me');
     return response.data;
